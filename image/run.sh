2f56601922cf67adfd4fbd5fe8116d87384335b2
Matteo Francia <m.francia@unibo.it>
2025-02-04 18:09:06 +0100

    fix: add rocks db

--- image/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -exo
-cd src/
-jupyter nbconvert --execute --to notebook --inplace notebook.ipynb
--- release.config.js
+++ release.config.js
@@ -1,16 +1,10 @@
 const publishCmd = `
-chmod +x *.sh; ./publish.sh \${nextRelease.version} w4bo python
+chmod +x *.sh; ./publish.sh \${nextRelease.version} w4bo rocksdb
 `;
 const config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-    ["@semantic-release/exec",
-        {
-            "publishCmd": publishCmd,
-        }],
-    ["@semantic-release/github",
-        {
-            "assets": [ /* {"path": "welaser/testScripts/*.pdf", "label": "Charts"} */]
-        }],
+    ["@semantic-release/exec", { "publishCmd": publishCmd, }],
+    ["@semantic-release/github", {}],
     "@semantic-release/git"
 )
 module.exports = config
