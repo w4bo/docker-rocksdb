723c5785bd9f2f3a3e087407907df506df3bf365
Matteo Francia <m.francia@unibo.it>
2025-02-06 09:46:27 +0100

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -4,7 +4,9 @@ chmod +x *.sh; ./publish.sh \${nextRelease.version} w4bo rocksdb
 const config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
     ["@semantic-release/exec", { "publishCmd": publishCmd, }],
-    ["@semantic-release/github", [{"path": "image/java-libs/*.jar", "label": "jar"}, {"path": "image/java-libs/*.so", "label": "so"}]],
+    ["@semantic-release/github", {
+        "assets": ["image/java-libs/*.jar", "image/java-libs/*.so"]
+    }],
     "@semantic-release/git"
 )
 module.exports = config
